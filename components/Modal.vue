120ac095ec7fb2736d7629a6cf3eab0f16f93340
s.shimizu <s-shimizu@tsumikiinc.com>
2022-10-07 13:57:02 +0900

    first commit.

---
+++ components/Modal.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+.modal.fixed.w-screen.h-screen.top-0.left-0.z-10
+  .modal__body.container.w-96.h-40.absolute.inset-0.m-auto.z-30.p-8.bg-white.rounded-lg
+    input.modal__input.pointer-events-auto.border-solid.border-2.border-black-100.p-2(
+      ref='barcode'
+      v-model='barcodeData'
+      )
+    button.pointer-events-auto.px-4.py-2.bg-blue-900.text-white.ml-2.rounded-lg(@click='close')
+      |close
+    div
+      |入力されたデータ
+      |{{ barcodeData }}
+  .modal__bg.absolute.w-full.h-full.top-0.left-0.z-20.bg-black.opacity-50(
+    @click='$emit("close")'
+  )
+</template>
+  
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Modal',
+  data: () => ({
+    barcodeData: '',
+  }),
+  methods: {
+    setFocus() {
+      const refs: any = this.$refs
+      refs.barcode.focus()
+    },
+    close() {
+      this.barcodeData = ''
+      this.$emit("close")
+    }
+  }
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+div 
+  h1 test site
+  button.px-4.py-2.bg-blue-900.text-white.rounded-lg(
+    @click='openModal'
+  )
+    |open Modal
+  transition(name="fade")
+    Modal(
+      ref='child'
+      v-show='isModal'
+      @close='closeModal'
+    )
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Modal from '~/components/Modal.vue';
+
+export default Vue.extend({
+  name: "IndexPage",
+  components: { Modal },
+  data: () => ({
+    isModal: false,
+  }),
+  methods: {
+    openModal() {
+      this.isModal = true
+      this.$nextTick(() => {
+        const refs: any = this.$refs
+        refs.child.setFocus()
+      })
+    },
+    closeModal() {
+      this.isModal = false
+    }
+  }
+})
+</script>
+
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
